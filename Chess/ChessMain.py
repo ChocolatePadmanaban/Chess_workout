9d777c1101654ea9f9108ea45454f1393c838d83
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2021-03-08 14:45:52 +0530

    Moving_piece completed

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -33,10 +33,30 @@ def main():
     gs = ChessEngine.GameState()
     loadImages() #only do this once, before the while loop 
     running=True
+    sqSelected = () #no square is selected , keep track of the last click of the user (tuple: row, col)
+    playerClicks = [] # keep track of player clicks (two tuples: [(6,4),(4,4)]
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #(x,y) location of the mouse 
+                col = location[0]// SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): # the user clicked the same square twice
+                    sqSelected= () #deselected 
+                    playerClicks=[]
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected) # append for both 1st and 2nd clicks 
+                if len(playerClicks)==2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1],gs.board)
+                    print(move.getChessNotation() )
+                    gs.makeMove(move)
+                    sqSelected= () #reset user clicks 
+                    playerClicks = []
+
         drawGameState(screen,gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -45,7 +65,8 @@ def drawGameState(screen, gs):
     """
     Responsible for all the graphics within a current game state.
     """
-    drawBoard(screen)# draw squares on the board 
+    drawBoard(screen)
+    # draw squares on the board 
     # add in piece highlighting or move suggestion (later)
     drawPieces(screen, gs.board)
 
